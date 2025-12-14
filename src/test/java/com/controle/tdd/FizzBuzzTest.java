4b08227829c3fbf4bf8b0eb06ba76d2b3dbc3d59
Jamila033 <jamilaallali2003@gmail.com>
2025-12-14 23:36:28 +0100

    Cycle 4 : FizzBuzz.de(6) à "Fizz"

--- src/test/java/com/controle/tdd/FizzBuzzTest.java
+++ src/test/java/com/controle/tdd/FizzBuzzTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 public class FizzBuzzTest {
     @Test
-    void test_de_5_devrait_retourner_Buzz() {
-        assertThat(FizzBuzz.de(5)).isEqualTo("Buzz");
+    void test_de_6_devrait_retourner_Fizz() {
+        assertThat(FizzBuzz.de(6)).isEqualTo("Fizz");
     }
 }
