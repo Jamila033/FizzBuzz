1a138e2f8b899e4bd5bef529ca64b3fddae8365d
Jamila033 <jamilaallali2003@gmail.com>
2025-12-14 23:44:38 +0100

    Cycle 5 : FizzBuzz.de(15) à "FizzBuzz"

--- src/main/java/com/controle/tdd/FizzBuzz.java
+++ src/main/java/com/controle/tdd/FizzBuzz.java
@@ -3,12 +3,15 @@ package com.controle.tdd;
 public class FizzBuzz {
 
     public static String de(int nbre) {
-        if (nbre % 3 == 0) {
+        if (nbre % 3 == 0 && nbre % 5 == 0) { // Condition 1 : La plus spécifique
+            return "FizzBuzz";
+        }
+        if (nbre % 3 == 0) { // Condition 2 : Reste des multiples de 3
             return "Fizz";
         }
-        if (nbre % 5 == 0) {
+        if (nbre % 5 == 0) { // Condition 3 : Reste des multiples de 5
             return "Buzz";
         }
-        return String.valueOf(nbre);
+        return String.valueOf(nbre); // Condition 4 : Cas par défaut
     }
 }
--- src/test/java/com/controle/tdd/FizzBuzzTest.java
+++ src/test/java/com/controle/tdd/FizzBuzzTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 public class FizzBuzzTest {
     @Test
-    void test_de_6_devrait_retourner_Fizz() {
-        assertThat(FizzBuzz.de(6)).isEqualTo("Fizz");
+    void test_de_15_devrait_retourner_FizzBuzz() {
+        assertThat(FizzBuzz.de(15)).isEqualTo("FizzBuzz");
     }
 }
