2efdaee50ae09340597838f072be8704703bb43b
Jamila033 <jamilaallali2003@gmail.com>
2025-12-14 23:27:11 +0100

    Cycle 1 : FizzBuzz.de(1) à "1"

---
+++ src/main/java/com/controle/tdd/FizzBuzz.java
@@ -0,0 +1,12 @@
+package com.controle.tdd;
+
+public class FizzBuzz {
+
+    public static String de(int nbre) {
+
+            if (nbre == 1) {
+                return "1";
+            }
+            return "";
+    }
+}
